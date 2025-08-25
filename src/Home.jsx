e10035fcbeeca223dcb8d8ca330a5c092cefb86b
lisazumana03 <zumanalisa@gmail.com>
2025-08-25 22:16:28 +0200

    file coming

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,12 @@
+import {Link} from 'react-router-dom'
+
 function Home(){
     return (
         <div>
             <p> The number one place to book for birthday parties or gaming cons. </p>
+            <Link to="/make-booking"> Create Booking </Link>
+            <Link to="/games"> View Available Games </Link>
+            <Link to="/consoles"> View Available Consoles </Link>
         </div>
     )
 }
